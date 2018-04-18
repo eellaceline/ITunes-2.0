1d8cd0fa20cf43ba8e502ee1d943975095fbd14e
RalleOlsson <R.olsson96@gmail.com>
2018-04-18 15:35:16 +0200

    V0.001

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,21 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable{
+
+    @FXML private AnchorPane rootPane;
 
     @FXML private Button userButton;
 
@@ -23,4 +29,10 @@ public class Controller implements Initializable{
         Image image = new Image(getClass().getResourceAsStream("photos/logo.png"));
         logoView.setImage(image);
     }
+
+    public void changeUserScene() throws Exception {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("sample2.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
 }
