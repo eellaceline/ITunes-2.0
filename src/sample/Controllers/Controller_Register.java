7582a94baf322a9f7ffc78437bbc41bc8c6fac5b
eellaceline <38213946+eellaceline@users.noreply.github.com>
2018-05-21 20:16:19 +0200

    Ellas del

--- src/sample/Controllers/Controller_Register.java
+++ src/sample/Controllers/Controller_Register.java
@@ -212,7 +212,8 @@ public class Controller_Register implements Initializable {
 
                 Handler_SendEmail.sendMail(
                         "Account created",
-                        "Dear newly customer, you have created an account at Soundwave."
+                        "Dear newly customer, you have created an account at Soundwave." +
+                                "\nAn email has been sent to you as a comfirmation."
                 );
 
                 paneChangeToLogin();
